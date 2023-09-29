3635a2f0944e55c849da12880899eb866d4e5f67
Admin <Admin@TEB123011>
2023-09-29 14:35:37 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace uppgift_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur långt kunde du hoppa Elin?");
+            string elin = Console.ReadLine();
+            int hoplängd = int.Parse(elin);
+            Console.WriteLine("hur långt kunde du hoppa Alma?");
+            string alma = Console.ReadLine();
+            int hopplängd2 = int.Parse(alma);
+            int skillnaden = hoplängd - hopplängd2;
+            Console.WriteLine("Elin hoppade " + elin + " meter lång och alma hoppade " + alma + " meter lång och skillnaden mellan de är " + skillnaden);
+        }
+    }
+}
